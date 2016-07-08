11857ba63fc25a873463ef96ca55fa7d206af047
KevinKuang <kuangweike@gmail.com>
2016-07-08 15:03:11 +0800

    'add'

--- dist/static/js/main.js
+++ dist/static/js/main.js
@@ -4,8 +4,9 @@ define('js/main', function(require, exports, module) {
   let $ctn = $('#container');
   let b = new B($ctn, 12, 10, 30);
   
-  $ctn.swipeDown(function() {
-      b.down();
-  })
+  $ctn.swipeDown((e)=>{e.stopPropagation();b.down()})
+      .swipeUp((e)=>{e.stopPropagation();b.up()})
+      .swipeLeft((e)=>{e.stopPropagation();b.left()})   
+      .swipeRight((e)=>{e.stopPropagation();b.right()})
 
 });
