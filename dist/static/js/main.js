32bb70cbce9f98d59610fdcd3299634b3631ce62
KevinKuang <kuangweike@gmail.com>
2016-07-08 15:13:30 +0800

    change folder

--- dist/static/js/main.js
+++ dist/static/js/main.js
@@ -4,9 +4,15 @@ define('js/main', function(require, exports, module) {
   let $ctn = $('#container');
   let b = new B($ctn, 12, 10, 30);
   
-  $ctn.swipeDown((e)=>{e.stopPropagation();b.down()})
-      .swipeUp((e)=>{e.stopPropagation();b.up()})
-      .swipeLeft((e)=>{e.stopPropagation();b.left()})   
-      .swipeRight((e)=>{e.stopPropagation();b.right()})
+  function prevent(e) {
+      e.stopPropagation();
+      e.preventDefault();
+  }
+  
+  
+  $ctn.swipeDown((e)=>{prevent();b.down()})
+      .swipeUp((e)=>{prevnt(); b.up()})
+      .swipeLeft((e)=>{prevnt(); b.left()})   
+      .swipeRight((e)=>{prevnt(); b.right()})
 
 });
--- fis-conf.js
+++ fis-conf.js
@@ -1,5 +1,6 @@
 var releaseUrl = '/eluosi-bricks/dist/'
 // 启用 es6-babel 插件，解析 .es6 后缀为 .js
+fis.project.setProjectRoot('src');
 fis.match('*.es6', {
   rExt: '.js',
   parser: fis.plugin('es6-babel')
