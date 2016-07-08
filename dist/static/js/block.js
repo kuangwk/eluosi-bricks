632baa64eb4fed7fab4d57262ef5982c5a4e64a4
KevinKuang <kuangweike@gmail.com>
2016-07-08 17:06:42 +0800

    c

--- dist/static/js/block.js
+++ dist/static/js/block.js
@@ -2,6 +2,10 @@ define('js/block', function(require, exports, module) {
 
   let Brick = require('js/brick');
   
+  function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+  
   var Maps = {
       'line': [{x: 10, y: 0}, {x: 11, y: 0}, {x: 12, y: 0}, {x: 13, y: 0}],
       'z': [{x: 10, y: 0}, {x: 11, y: 0}, {x: 11, y: 1}, {x: 12, y: 1}],
@@ -13,7 +17,7 @@ define('js/block', function(require, exports, module) {
   class Block {
       constructor($ctn) {
           this.$ctn = $ctn;
-          this.bricks = this.getBricks(Keys[3]);
+          this.bricks = this.getBricks(Keys[getRandomInt(0, 3)]);
       }
       getBricks(name) {
           return Maps[name].map((pos)=> new Brick(this.$ctn, pos.x, pos.y));
--- src/js/block.js
+++ src/js/block.js
@@ -1,5 +1,9 @@
 let Brick = require('./brick');
 
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
 var Maps = {
     'line': [{x: 10, y: 0}, {x: 11, y: 0}, {x: 12, y: 0}, {x: 13, y: 0}],
     'z': [{x: 10, y: 0}, {x: 11, y: 0}, {x: 11, y: 1}, {x: 12, y: 1}],
@@ -11,7 +15,7 @@ var Keys = ['line', 'z', 'tian', 'qi'];
 class Block {
     constructor($ctn) {
         this.$ctn = $ctn;
-        this.bricks = this.getBricks(Keys[3]);
+        this.bricks = this.getBricks(Keys[getRandomInt(0, 3)]);
     }
     getBricks(name) {
         return Maps[name].map((pos)=> new Brick(this.$ctn, pos.x, pos.y));
