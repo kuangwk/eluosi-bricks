2b99870c5f2f4277ac971415d2400586a381c04e
KevinKuang <kuangweike@gmail.com>
2016-07-08 15:52:32 +0800

    2

--- src/js/main.js
+++ src/js/main.js
@@ -2,15 +2,13 @@ let B = require('./brick');
 let $ctn = $('#container');
 let b = new B($ctn, 12, 10, 30);
 
-$('body').on('touchmove', function (event) {event.preventDefault();});
-
 function prevent(e) {
     e.stopPropagation();
     e.preventDefault();
 }
 
-
 $ctn.swipeDown((e)=>{prevent(e);b.down()})
     .swipeUp((e)=>{prevent(e); b.up()})
     .swipeLeft((e)=>{prevent(e); b.left()})   
     .swipeRight((e)=>{prevent(e); b.right()})
+    .on('click singleTap dblclick touchmove', prevent);
